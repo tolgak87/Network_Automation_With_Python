f7d004ff0cd306479c8ec805e353c2da3b34dc43
tolgak87 <koca.tolga@gmail.com>
2020-02-14 13:34:51 +0300

    Update ping.py

--- ping.py
+++ ping.py
@@ -1,12 +1,18 @@
 import subprocess
-reached = []                                                #Empty list to collect reachable hosts
-not_reached = []                                            #Empty list to collect unreachable hosts
+
+def ping_test (hosts):                                          #Create ping_test function
+    reached = []                                                #Empty list to collect reachable hosts
+    not_reached = []                                            #Empty list to collect unreachable hosts
+    
+    for ip in hosts:
+        ping_test = subprocess.call('ping %s -n 2' % ip)        #Ping host n times
+        if ping_test == 0:                                      #If ping test is 0, it' reachable
+            reached.append(ip)
+        else:
+            not_reached.append(ip)                              #Else, it's not reachable
+    print("{} is reachable".format(reached))
+    print("{} not reachable".format(not_reached))
+
+
 hosts = ["192.168.1.1","123.214.2.2","www.google.com",]     #Hosts list
-for ip in hosts:
-    ping_test = subprocess.call('ping %s -n 2' % ip)        #Ping host n times
-    if ping_test == 0:                                      #If ping test is 0, it' reachable
-        reached.append(ip)
-    else:
-        not_reached.append(ip)                              #Else, it's not reachable
-print("{} is reachable".format(reached))
-print("{} not reachable".format(not_reached))
+ping_test (hosts)                                           #Call ping_test function
